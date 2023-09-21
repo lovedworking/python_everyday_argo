40ac5b5d84ced5598ad75b34034a6ef590a45110
lovework <tukunkun@wisers.com>
2023-09-21 23:04:52 +0800

    Z tag convert

---
+++ top100/6zig_tag_conversion/convert_soulution.py
@@ -0,0 +1,36 @@
+"""
+回头再搞剑指offer
+https://www.nowcoder.com/exam/oj/ta?page=1&tpId=13&type=13
+
+注意只有在0行 numRows-1 变换方向向 上边界变为加   下边界变为减
+最终只保留每一行对应一个字符串
+
+
+"""
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2:
+            return s
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1:
+                flag = -flag
+            i += flag
+        return "".join(res)
+
+sol = Solution()
+print(sol.convert("PAYPALISHIRING", 3))
+
+
+
+
+
+
+
+
+
+
+
