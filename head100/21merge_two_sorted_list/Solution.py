e90f66d6a97a3b5365997945139a3552918ceda5
lovework <tukunkun@wisers.com>
2023-10-03 23:21:34 +0800

    21 merge two sorted list

---
+++ head100/21merge_two_sorted_list/Solution.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        preNode = ListNode(-1)
+        head = preNode
+        while list1 and list2:
+            if list1.val <= list2.val:
+                preNode.next = list1
+                list1 = list1.next
+            else:
+                preNode.next = list2
+                list2 = list2.next
+            preNode = preNode.next
+        preNode.next = list1 if list1 is not None else list2
+
+        return head.next
+
+
+sol = Solution()
+sol.mergeTwoLists([1,3,5],[2,4])
